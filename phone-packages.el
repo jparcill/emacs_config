24dae177a7131d1413d410edbd97265c899d4c43
JaredArcilla <jparcill@uwaterloo.ca>
2021-05-29 15:07:09 -0400

    phone settings

---
+++ phone-packages.el
@@ -0,0 +1,26 @@
+;; -*- no-byte-compile: t; -*-
+;;; .doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+(package! org-super-agenda)
+(package! browse-kill-ring)
+;;(package! exwm)
+;;(package! org-download)
+;;(package! ox-hugo)
+;;(package! polymode)
+;;(package! zmq)
+;;(package! shrface)
+(package! counsel-org-clock)
+;;(package! exwm-firefox :recipe
+;;  (:host github :repo "ieure/exwm-firefox"))
+(package! olivetti)
+;;(package! counsel-spotify)
+;;(package! lsp-julia :recipe (:host github :repo "non-jedi/lsp-julia"))
+;;(package! exwm-edit)
+;;(package! nov)
+;;(package! mathpix.el :recipe (:host github :repo "jethrokuan/mathpix.el"))
+;;(package! dap-mode)
